6161f7588be8f1492051dc53e8e7c65761a7b901
gudrl92 <sswwx1@naver.com>
2019-05-13 17:58:13 +0900

    경기장 dto 추가

---
+++ src/main/java/com/fm/www/dto/Ground.java
@@ -0,0 +1,43 @@
+package com.fm.www.dto;
+
+public class Ground {
+
+	private int board_fg;
+	private int board_no;
+	private String ground_addr;
+	private String ground_name;
+	@Override
+	public String toString() {
+		return "Ground [board_fg=" + board_fg + ", board_no=" + board_no + ", ground_addr=" + ground_addr
+				+ ", ground_name=" + ground_name + "]";
+	}
+	public int getBoard_fg() {
+		return board_fg;
+	}
+	public void setBoard_fg(int board_fg) {
+		this.board_fg = board_fg;
+	}
+	public int getBoard_no() {
+		return board_no;
+	}
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+	public String getGround_addr() {
+		return ground_addr;
+	}
+	public void setGround_addr(String ground_addr) {
+		this.ground_addr = ground_addr;
+	}
+	public String getGround_name() {
+		return ground_name;
+	}
+	public void setGround_name(String ground_name) {
+		this.ground_name = ground_name;
+	}
+	
+	
+	
+	
+	
+}
